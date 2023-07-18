cd2d5d7a88364ae0e9d7f12415a52f80a93ba878
André Baill <srandrebaill@gmail.com>
2023-07-18 09:33:51 -0300

    Configuração embed Talkto

--- site/views/view_rodape.php
+++ site/views/view_rodape.php
@@ -31,7 +31,21 @@
 
         gtag('config', 'G-GFHLV9RNRB');
     </script>
-
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+        var Tawk_API = Tawk_API || {},
+            Tawk_LoadStart = new Date();
+        (function() {
+            var s1 = document.createElement("script"),
+                s0 = document.getElementsByTagName("script")[0];
+            s1.async = true;
+            s1.src = 'https://embed.tawk.to/64b6867ccc26a871b02925cc/1h5kfqmc1';
+            s1.charset = 'UTF-8';
+            s1.setAttribute('crossorigin', '*');
+            s0.parentNode.insertBefore(s1, s0);
+        })();
+    </script>
+    <!--End of Tawk.to Script-->
 
     </body>
 
